ffa6d77af685a9728305266c3e8f6112d23b88e6
Dev-Alaf-Ferreira <slypknot1@hotmail.com>
2024-06-19 08:55:33 -0300

    alteracoes_escopo

--- 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
+++ 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
@@ -4,7 +4,7 @@ def somar(a, b):
 
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
-    print(f"O resultado da operação {a} + {b} = {resultado}")
+    print(f"O resultado da operação é = {resultado}")
 
 
-exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
+exibir_resultado(15, 10, somar)  # O resultado da operação 10 + 10 = 20
